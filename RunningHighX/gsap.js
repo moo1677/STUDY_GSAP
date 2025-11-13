cd0608dc177d567edea4b2a66fb0b373637d9eb8
moo1677 <lgm04@naver.com>
2025-11-13 21:05:37 +0900

    style: GSAP 메서드에 class naming convention 적용

--- RunningHighX/gsap.js
+++ RunningHighX/gsap.js
@@ -1,8 +1,8 @@
 const intro = gsap.timeline();
 intro
-  .to(".x-bar", { scale: 12, backgroundColor: "#000", autoAlpha: 0 })
+  .to(".intro__x-bar", { scale: 12, backgroundColor: "#000", autoAlpha: 0 })
   .to("#running-high-intro", { backgroundColor: "#000" }, "<")
-  .to(".shoe", { autoAlpha: 0 }, "<");
+  .to(".intro__shoe", { autoAlpha: 0 }, "<");
 ScrollTrigger.create({
   animation: intro,
   trigger: "#running-high-intro",
@@ -29,10 +29,22 @@ ScrollTrigger.create({
 
 const shoesDeco = gsap.timeline();
 shoesDeco
-  .to(".upper", { x: "-=300", y: -100, rotation: 10 }, "<")
-  .to(".insole", { x: "-=300", y: "+=120", rotation: 10 }, "<")
-  .to(".outsole", { x: "-=300", y: "+=350", rotation: 10 }, "<")
-  .from(".shoes-info", { y: 50, autoAlpha: 0, stagger: 0.1 });
+  .to(
+    ".features__image-part--upper",
+    { x: "-=300", y: -100, rotation: 10 },
+    "<"
+  )
+  .to(
+    ".features__image-part--insole",
+    { x: "-=300", y: "+=200", rotation: 10 },
+    "<"
+  )
+  .to(
+    ".features__image-part--outsole",
+    { x: "-=300", y: "+=490", rotation: 10 },
+    "<"
+  )
+  .from(".info-card", { y: 50, autoAlpha: 0, stagger: 0.1 });
 ScrollTrigger.create({
   animation: shoesDeco,
   trigger: "#shoe-features",
@@ -46,7 +58,7 @@ ScrollTrigger.create({
 const upper = gsap.timeline();
 upper
   .from("#upper", { y: "+=100", autoAlpha: 0 })
-  .from(".upper-body", { y: "+=50", autoAlpha: 0 }, "+=0.2<");
+  .from(".detail__body--upper", { y: "+=50", autoAlpha: 0 }, "+=0.01<");
 ScrollTrigger.create({
   animation: upper,
   trigger: "#feature-upper",
@@ -56,7 +68,7 @@ ScrollTrigger.create({
 const insole = gsap.timeline();
 insole
   .from("#insole", { y: "+=100", autoAlpha: 0 })
-  .from(".insole-body", { y: "+=50", autoAlpha: 0 }, "+=0.2<");
+  .from(".detail__body--insole", { y: "+=50", autoAlpha: 0 }, "+=0.01<");
 ScrollTrigger.create({
   animation: insole,
   trigger: "#feature-insole",
@@ -66,7 +78,7 @@ ScrollTrigger.create({
 const outsole = gsap.timeline();
 outsole
   .from("#outsole", { y: "+=100", autoAlpha: 0 })
-  .from(".outsole-body", { y: "+=50", autoAlpha: 0 }, "+=0.2<");
+  .from(".detail__body--outsole", { y: "+=50", autoAlpha: 0 }, "+=0.01<");
 ScrollTrigger.create({
   animation: outsole,
   trigger: "#feature-outsole",
@@ -76,14 +88,14 @@ ScrollTrigger.create({
 
 const weightShoes = gsap.timeline();
 weightShoes
-  .from(".weight-title", { y: "+=100", autoAlpha: 0 })
-  .to(".weight-title", { autoAlpha: 0 })
+  .from(".weight__title", { y: "+=100", autoAlpha: 0 })
+  .to(".weight__title", { autoAlpha: 0 })
   .from("#shoe", { autoAlpha: 0 }, "<")
   .to("#shoe", { autoAlpha: 0 })
   .from("#feather", { autoAlpha: 0 }, "-=0.3")
   .to("#feather", { rotation: "+=10", scale: 1.1, ease: "back.out" })
   .from("#grass", { y: "+=1000", autoAlpha: 0 })
-  .from(".weight-text", { autoAlpha: 0 })
+  .from(".weight__text", { autoAlpha: 0 })
   .to("#grass", { autoAlpha: 0 }, "+=1")
   .to("#feather", { autoAlpha: 0 }, "<");
 
@@ -105,8 +117,8 @@ ScrollTrigger.create({
 const endShoe = gsap.timeline();
 endShoe
   .from("#end-shoe", { x: -10000, duration: 0.5 })
-  .from(".end-info", { x: 100, autoAlpha: 0 })
-  .from(".end-text", { y: 100, autoAlpha: 0 }, "<");
+  .from(".summary__info-wrapper", { x: 100, autoAlpha: 0 })
+  .from(".summary__text", { y: 100, autoAlpha: 0 }, "<");
 ScrollTrigger.create({
   animation: endShoe,
   trigger: "#running-high-end",
